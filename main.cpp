67b0b8454f1f65c4940fad16bff3d97288d6234f
MGalas <48221141+GalasM@users.noreply.github.com>
2019-03-23 13:06:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,14 @@
 #include<cstdlib>
 #include<chrono>
 #include<fstream>
+#include "main.h"
+
 //ZMIENNE GLOBALNE
 unsigned long long loop_counter,changes_counter,I;
 
 using namespace std;
 
-//DEKLARACJE FUNKCJI
-void filltab(double *,unsigned long long N);
-void  bubbleSortPerf(double *,unsigned long long );
-void  bubbleSort(double *,unsigned long long );
-void printArray(double *,unsigned long long );
-void coppyArray(double *,double *,unsigned long long );
-void bubbleSortReverse(double *,unsigned long long );
-void FillFile(double *tab,unsigned long long N);
-void loadFromFile(double *t);
+
 
 int main()
 {

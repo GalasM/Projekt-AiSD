7387e141a0807708754468073c8d13f78c8ce9d4
MGalas <48221141+GalasM@users.noreply.github.com>
2019-03-23 13:06:36 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,10 @@
 
+//DEKLARACJE FUNKCJI
+void filltab(double *,unsigned long long N);
+void  bubbleSortPerf(double *,unsigned long long );
+void  bubbleSort(double *,unsigned long long );
+void printArray(double *,unsigned long long );
+void coppyArray(double *,double *,unsigned long long );
+void bubbleSortReverse(double *,unsigned long long );
+void FillFile(double *tab,unsigned long long N);
+void loadFromFile(double *t);
